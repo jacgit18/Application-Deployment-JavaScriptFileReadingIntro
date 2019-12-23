fb2f493138a086b41019947d830b5932a1b1525a
jac <joshuacarpentier21@gmail.com>
2019-12-22 19:35:44 -0500

    adde code

---
+++ examRepository.js
@@ -0,0 +1,42 @@
+/**
+ * objective to read a JSON file
+ */
+
+ function findAll(fileName, callback){
+
+
+
+var readline = require('readline');
+var fs = require('fs');
+var rows = []
+
+var myInterface = readline.createInterface({
+  input: fs.createReadStream(fileName)
+  
+});
+
+// call back so it runs on a delay and may get skipped because of
+// this but will still run
+myInterface.on('line', line => {
+  const json = JSON.parse(line)
+  rows.push(json)
+});
+
+
+// callback is just a valid vairiable name
+myInterface.on('close', line => {
+  callback(rows)
+});
+
+}
+
+findAll("73-data.json", e => {
+let excellentGrades = e.filter( e => e.avg >90);
+
+console.log(excellentGrades)
+
+let summary = excellentGrades.map(r => r.avg)
+               .reduce ((a,c) => a + c, 0) / excellentGrades.length;
+console.log({summary, numberOfStudents: excellentGrades.length})
+})
+// findAll("73-data.json", e => console.log(e))
